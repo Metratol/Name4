d396872f6974001dfecc90eb4014c48d73d66d62
Metra <metratoll@gmail.com>
2023-11-16 13:56:49 +0300

    Update4

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Изменение 1");
         System.out.println("Изменение 2");
         System.out.println("Изменение 3");
+        System.out.println("Изменение 4");
     }
 }

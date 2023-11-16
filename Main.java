5e6073b2bda209cdf3840e32e4269a66c40324dd
Metra <metratoll@gmail.com>
2023-11-16 13:50:16 +0300

    Update2

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке dev");
         System.out.println("Изменение 1");
+        System.out.println("Изменение 2");
     }
 }
